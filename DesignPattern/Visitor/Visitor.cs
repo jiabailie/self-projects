7e3f0110aae398930cf843afdee97953693c18fb
rgyang <rgyang@ctrip.com>
2014-02-21 14:54:41 +0800

    add design pattern/visitor

---
+++ DesignPattern/Visitor/Visitor.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections;
+
+abstract public class Visitor
+{
+    abstract public void VisitConcreteElementA(ConcreteElementA concreteElementA);
+
+    abstract public void VisitConcreteElementB(ConcreteElementB concreteElementB);
+}
+
+public class ConcreteVisitor1 : Visitor
+{
+    public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+    {
+        Console.WriteLine("{0} is visited by {1}", concreteElementA, this);
+    }
+
+    public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+    {
+        Console.WriteLine("{0} is visited by {1}", concreteElementB, this);
+    }
+}
+
+public class ConcreteVisitor2 : Visitor
+{
+    public override void VisitConcreteElementA(ConcreteElementA concreteElementA)
+    {
+        Console.WriteLine("{0} is visited by {1}", concreteElementA, this);
+    }
+
+    public override void VisitConcreteElementB(ConcreteElementB concreteElementB)
+    {
+        Console.WriteLine("{0} is visited by {1}", concreteElementB, this);
+    }
+}
+
+abstract public class Element
+{
+    abstract public void Accept(Visitor visitor);
+}
+
+public class ConcreteElementA : Element
+{
+    public override void Accept(Visitor visitor)
+    {
+        visitor.VisitConcreteElementA(this);
+    }
+
+    public void OperationA() { }
+}
+
+public class ConcreteElementB : Element
+{
+    public override void Accept(Visitor visitor)
+    {
+        visitor.VisitConcreteElementB(this);
+    }
+
+    public void OperationB() { }
+}
+
+public class ObjectStructure
+{
+    private ArrayList elements = new ArrayList();
+
+    public void Attach(Element element)
+    {
+        elements.Add(element);
+    }
+
+    public void Detach(Element element)
+    {
+        elements.Remove(element);
+    }
+
+    public void Accept(Visitor visitor)
+    {
+        foreach (Element e in elements)
+        {
+            e.Accept(visitor);
+        }
+    }
+}
+
+public class Client
+{
+    public Client()
+    {
+        ObjectStructure o = new ObjectStructure();
+        o.Attach(new ConcreteElementA());
+        o.Attach(new ConcreteElementB());
+
+        ConcreteVisitor1 v1 = new ConcreteVisitor1();
+        ConcreteVisitor2 v2 = new ConcreteVisitor2();
+
+        o.Accept(v1);
+        o.Accept(v2);
+    }
+}
+    
---
+++ DesignPattern/Visitor/VisitorApp.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+
+abstract public class Visitor
+{
+    abstract public void Visit(Element element);
+}
+
+public class IncomeVisitor : Visitor
+{
+    public override void Visit(Element element)
+    {
+        Employee employee = (Employee)element;
+
+        employee.Income *= 10;
+        Console.WriteLine("{0}'s new income:{1:C}", employee.Name, employee.Income);
+    }
+}
+
+public class VacationVisitor : Visitor
+{
+    public override void Visit(Element element)
+    {
+        Employee employee = (Employee)element;
+
+        employee.VacationDays += 3;
+        Console.WriteLine("{0}'s new vacation days:{1}", employee.Name, employee.VacationDays);
+    }
+}
+
+abstract public class Element
+{
+    abstract public void Accept(Visitor visitor);
+}
+
+public class Employee : Element
+{
+    public string Name { get; set; }
+    public double Income { get; set; }
+    public int VacationDays { get; set; }
+
+    public Employee(string name, double income, int vacationDays)
+    {
+        Name = name;
+        Income = income;
+        VacationDays = vacationDays;
+    }
+
+    public override void Accept(Visitor visitor)
+    {
+        visitor.Visit(this);
+    }
+}
+
+public class Employees
+{
+    private ArrayList employees = new ArrayList();
+
+    public void Attach(Employee employee)
+    {
+        employees.Add(employee);
+    }
+
+    public void Detach(Employee employee)
+    {
+        employees.Remove(employee);
+    }
+
+    public void Accept(Visitor visitor)
+    {
+        foreach (Employee e in employees)
+        {
+            e.Accept(visitor);
+        }
+    }
+}
+
+public class VisitorApp
+{
+    public VisitorApp()
+    {
+        Employees e = new Employees();
+        e.Attach(new Employee("Hank", 25000, 14));
+        e.Attach(new Employee("Elly", 35000, 16));
+        e.Attach(new Employee("Dick", 45000, 21));
+
+        IncomeVisitor v1 = new IncomeVisitor();
+        VacationVisitor v2 = new VacationVisitor();
+
+        e.Accept(v1);
+        e.Accept(v2);
+    }
+}
